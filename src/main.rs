f023b0e73d2473e126e4fdac46fdc6f402da1888
KaiserLancelot <1244713586@qq.com>
2020-05-12 10:40:54 +0800

    2020.5.12

---
+++ src/main.rs
@@ -0,0 +1,10 @@
+/**
+ * @ Author: KaiserLancelot
+ * @ Create Time: 2020-05-12 10:28:29
+ * @ Modified by: KaiserLancelot
+ * @ Modified time: 2020-05-12 10:38:58
+ */
+
+fn main() {
+    println!("Hello World!");
+}
---
+++ test/test.rs
@@ -0,0 +1,6 @@
+/**
+ * @ Author: KaiserLancelot
+ * @ Create Time: 2020-05-12 10:38:37
+ * @ Modified by: KaiserLancelot
+ * @ Modified time: 2020-05-12 10:39:09
+ */
